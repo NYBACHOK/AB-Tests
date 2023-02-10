eb1a9582e55c9ff72eb2eaa689f1203af066f3fd
Nyba <nybachokua@gmail.com>
2023-02-10 14:22:25 +0200

    Add initial files

---
+++ Migrations/V1.0.0.1__add_experiments.sql
@@ -0,0 +1,14 @@
+﻿insert into experiments (expname) VALUES ('button_color');
+
+insert into experimentexamples (expid, optionname, optionvalue) VALUES 
+    ( 1, '#FF0000', 33), 
+    (1, '#00FF00', 33),
+    (1, '#0000FF', 33);
+
+insert into experiments (expname) VALUES ('price');
+
+insert into experimentexamples (expid, optionname, optionvalue) VALUES
+    ( 2, '10', 75),
+    (2, '20', 10),
+    (2, '50', 5),
+    (2, '5', 10);
